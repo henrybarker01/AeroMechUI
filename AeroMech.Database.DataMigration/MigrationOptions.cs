14c89c55276d02053fcad8bf7309108249bc1407
Henry Barker <henry.barker@live.com>
2026-08-16 15:41:44 +0200

    cleanup

--- AeroMech.Database.DataMigration/MigrationOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace AeroMech.Database.DataMigration
-{
-    public class MigrationOptions
-    {
-        public int BatchSize { get; set; } = 1000;
-        public bool SkipIdentityTables { get; set; } = true;
-    }
-}
